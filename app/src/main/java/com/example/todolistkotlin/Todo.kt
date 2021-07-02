8e9c9d7cac78ae2e2c428dd757122019f1fb7273
saurabhseth2495 <82148878+saurabhseth2495@users.noreply.github.com>
2021-07-02 18:52:39 +0530

    second commit

--- app/src/main/java/com/example/todolistkotlin/Todo.kt
+++ app/src/main/java/com/example/todolistkotlin/Todo.kt
@@ -5,5 +5,5 @@ import android.icu.text.CaseMap
 //data keyword is use with class where we don't write any logic, purpose to hold the data.
 data class Todo(
     val title: String,
-    var isChecked: Boolean = false
+    var isChecked:Boolean = false
 )
